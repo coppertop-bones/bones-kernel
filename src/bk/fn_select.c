336eb2b4c1afd98030821270be68004b71379225
DangerMouseB <dangermouseb@forwarding.cc>
2023-09-10 23:24:57 +0100

    add hc

--- src/bk/fn_select.c
+++ src/bk/fn_select.c
@@ -4,7 +4,6 @@
 
 #include "../../include/all.cfg"
 #include "../../include/bk/bk.h"
-
 #include "utils.c"
 
 
@@ -60,7 +59,7 @@ pvt void SC_at_array_put(struct SelectorCache *sc, int index, unsigned short sig
     dest[o_last] = 0x0000 | ((payload & LOWER_PAYLOAD_MASK) << LOWER_PAYLOAD_SHIFT);
 }
 
-pvt unsigned char SC_next_free_array_index(struct SelectorCache *sc) {
+pvt int SC_next_free_array_index(struct SelectorCache *sc) {
     int num_slots = sc->num_slots;
     int slot_width = sc->slot_width;
     unsigned short *array = P_SIG_ARRAY(sc);
@@ -68,28 +67,27 @@ pvt unsigned char SC_next_free_array_index(struct SelectorCache *sc) {
     return 0;
 }
 
-pvt inline fu16 fast_compare_sig(unsigned short query[], unsigned short sig[], fu8 slot_width) {
-    fu16 N = query[0];
+pvt inline int fast_compare_sig(unsigned short query[], unsigned short sig[], int slot_width, unsigned short *HC) {
+    int N = query[0];
     if (N != (sig[0] & LOWER_PAYLOAD_MASK)) return 0;                                       // check count
-    for (fu8 o = 1; o <= N; o++) {
+    for (int o = 1; o <= N; o++) {
         if (query[o] != sig[o]) return 0;                                        // check compressed bt_ids
 //        if (query[o] == 0) return (sig[0] & UPPER_PAYLOAD_MASK) | ((sig[o_last] >> LOWER_PAYLOAD_SHIFT) & LOWER_PAYLOAD_MASK);   // check null terminal
     }
 //    if (query[o_last] != (sig[o_last] & UPPER_PAYLOAD_MASK)) return 0;                             // check last
-    fu8 o_last = slot_width - 1;
-    unsigned int hc = (sig[o_last] & HC_MASK + HC_INC) & HC_MASK;
-    if (hc == 0) { // if wrapped inc off struct hit count
-
-    }
+    int o_last = slot_width - 1;
+    int hc = ((sig[o_last] & HC_MASK) + HC_INC) & HC_MASK;
+    if (hc == 0) *HC += 1;  // if wrapped inc off struct hit count
     sig[o_last] = hc | (sig[o_last] & NOT_HC_MASK);
     return (sig[0] & UPPER_PAYLOAD_MASK) | ((sig[o_last] >> LOWER_PAYLOAD_SHIFT) & LOWER_PAYLOAD_MASK);
 }
 
 // the client will likely probe array first, compute a hash if missing, then probe from hash start
 pvt int fast_probe_sigs(unsigned short query[], unsigned short sigs[], int slot_width, int num_slots) {
+    unsigned short *HCs = sigs + slot_width * num_slots;
     for (int o = 0; o < num_slots; o++) {
         if (*(sigs + o * slot_width) == 0) return 0;
-        int v = fast_compare_sig(query, sigs + o * slot_width, slot_width);
+        int v = fast_compare_sig(query, sigs + o * slot_width, slot_width, HCs + o);
         if (v) return v;
     }
     return 0;
@@ -98,10 +96,11 @@ pvt int fast_probe_sigs(unsigned short query[], unsigned short sigs[], int slot_
 pvt size_t SC_new_size(int num_args, int num_slots) {
     // OPEN check range and return err (like in SC_init)
     int slot_width = SLOT_WIDTH_FROM_NUM_ARGS(num_args);
-    return sizeof(struct SelectorCache) + sizeof(unsigned short) * (num_slots + 1) * slot_width;
+    return sizeof(struct SelectorCache) + sizeof(unsigned short) * (num_slots + 1) * slot_width + sizeof(unsigned short) * num_slots;
 }
 
 pvt err SC_init(struct SelectorCache *sc, int num_args, int num_slots) {
+    int i, iHC;
     int slot_width = SLOT_WIDTH_FROM_NUM_ARGS(num_args);
     if (!(1 <= num_args && num_args <=16)) SIGNAL("num_args is not within {1, 16}");         // OPEN add num_args value to msg
     if (!(1 <= num_slots && num_slots <=128)) SIGNAL("num_slots is not within {1, 128}");
@@ -109,9 +108,10 @@ pvt err SC_init(struct SelectorCache *sc, int num_args, int num_slots) {
     sc -> slot_width = slot_width;
     sc -> num_slots = num_slots;
     unsigned short *query = P_QUERY(sc);
-    for (int i=0; i < slot_width; i++) query[i] = 0x0000;
+    for (i=0; i < slot_width; i++) query[i] = 0x0000;
     unsigned short *array = P_SIG_ARRAY(sc);
-    for (int i=0; i < slot_width * num_slots; i++) array[i] = 0x0000;
+    for (i=0; i < slot_width * num_slots; i++) array[i] = 0x0000;
+    for (iHC=0; iHC < num_slots; iHC++) array[i+iHC] = 0x0000;
     return ok;
 }
 
